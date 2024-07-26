170ea31335583f868bdb8eefd93c6f47c7cd66c5
ajaykn_04 <ajaykn04@gmail.com>
2024-07-27 02:04:55 +0530

    make 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Login/>
     </>
   )
 }
