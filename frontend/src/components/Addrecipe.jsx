d80cb955a71a5ee9ef080a7755e0cdee85ef6cfd
ajaykn_04 <ajaykn04@gmail.com>
2024-08-01 00:20:27 +0530

    push 24

--- frontend/src/components/Addrecipe.jsx
+++ frontend/src/components/Addrecipe.jsx
@@ -1,39 +1,56 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material';
 import axios from 'axios';
-import React, { useState } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styles from '../styles';
 import Navbar from './Navbar';
 
 const Addrecipe = () => {
-  
-
   const location = useLocation();
-  console.log(location.state.username)
-  const [recipe, setRecipe] = useState({ name: "", ingredients: "", instructions: "", category: "", image: "",});
-  const [image, setImage] = useState();
-  const [errors, setErrors] = useState({ name: false, ingredients: false, instructions: false, category: false, image: false });
-  const [generalError, setGeneralError] = useState("");
-  
   const navigate = useNavigate();
 
-  const cred = {
-    name: recipe.name,
-    ingredients: recipe.ingredients,
-    instructions: recipe.instructions,
-    category: recipe.category,
-    image: recipe.image,
-    owner: location.state._id,
-    ownername: location.state.username,
+  const [recipe, setRecipe] = useState({
+    name: "", 
+    ingredients: "", 
+    instructions: "", 
+    category: "", 
+    image: "",
+  });
+  const [image, setImage] = useState();
+  const [errors, setErrors] = useState({
+    name: false, 
+    ingredients: false, 
+    instructions: false, 
+    category: false, 
+    image: false
+  });
+  const [generalError, setGeneralError] = useState("");
+
+  const handleInstructionsChange = (event) => {
+    const inputValue = event.target.value;
+    console.log(inputValue)
+    const lines = inputValue.split('\n');
+    console.log(lines)
+    const bulletedLines = lines.map((line, index) => {
+      if (index === lines.length - 1 && line === "• ") {
+        return "";
+      } else if (!line.startsWith('• ')) {
+        return `• ${line}`;
+      } else {
+        return line;
+      }
+    });
+    setRecipe(prevState => ({ ...prevState, instructions: bulletedLines.join('\n') }));
   };
 
-
   const inputHandler = (e) => {
-    setRecipe({ ...recipe, [e.target.name]: e.target.value });
-    setErrors({ ...errors, [e.target.name]: false });
-    if(e.target.type=="file"){
+    if (e.target.type === "file") {
       setImage(e.target.files[0]);
+      setRecipe({ ...recipe, image: e.target.files[0].name });
+    } else {
+      setRecipe({ ...recipe, [e.target.name]: e.target.value });
     }
+    setErrors({ ...errors, [e.target.name]: false });
     setGeneralError("");
   };
 
@@ -43,26 +60,28 @@ const Addrecipe = () => {
       ingredients: recipe.ingredients === "",
       instructions: recipe.instructions === "",
       category: recipe.category === "",
-      image: recipe.image === "",
+      image: !image,
     };
     setErrors(newErrors);
-    return !newErrors.name && !newErrors.ingredients && !newErrors.instructions && !newErrors.category && !newErrors.image;
+    return !Object.values(newErrors).some(error => error);
   };
 
   const submitHandler = async () => {
     if (validateFields()) {
       try {
         const data = new FormData();
-        data.append("file",image)
-        for (const key in cred) {
-          data.append(key, cred[key]);
-      }
+        data.append("file", image);
+        for (const key in recipe) {
+          data.append(key, recipe[key]);
+        }
+        data.append("owner", location.state._id);
+        data.append("ownername", location.state.username);
+
         await axios.post(`http://localhost:3000/recipe/add/`, data);
-        console.log("recipe added");
+        console.log("Recipe added");
         window.location.reload(true);
-        
       } catch (error) {
-          console.error(error);
+        console.error(error);
       }
     }
   };
@@ -70,7 +89,7 @@ const Addrecipe = () => {
   return (
     <div>
       <Navbar location={location} />
-        <Box
+      <Box
         sx={{
           display: 'flex',
           justifyContent: 'center',
@@ -79,40 +98,39 @@ const Addrecipe = () => {
         }}
       >
         <Box sx={styles.box_style}>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <img 
             src="/dishify_pbg1.ico" 
             alt="Login Icon" 
             style={{ width: '200px', marginBottom: '-1.5rem', marginTop: '-5rem' }} 
           />
-          <Typography fontFamily={'fantasy'} variant="h3" color="white" gutterBottom>
+          <Typography fontFamily={'fantasy'} variant="h4" color="white" gutterBottom>
             ADD RECIPE
           </Typography>
           <TextField
             required
             fullWidth
             name="name"
-            label="name"
+            label="Name"
             variant="outlined"
             margin="normal"
-            // value={recipe.name}
             onChange={inputHandler}
             error={errors.name}
-            helperText={errors.name ? 'name is required' : ''}
+            helperText={errors.name ? 'Name is required' : ''}
             InputLabelProps={{ style: { color: 'white' } }}
             InputProps={styles.textfield}
           />
           <TextField
             required
             fullWidth
+            multiline
+            rows={4}
             name="ingredients"
-            label="ingredients"
+            label="Ingredients"
             variant="outlined"
             margin="normal"
-            // value={recipe.ingredients}
             onChange={inputHandler}
             error={errors.ingredients}
-            helperText={errors.ingredients ? 'ingredients is required' : generalError}
+            helperText={errors.ingredients ? 'Ingredients are required' : generalError}
             FormHelperTextProps={{ sx: { color: 'red' } }}
             InputLabelProps={{ style: { color: 'white' } }}
             InputProps={styles.textfield}
@@ -120,14 +138,19 @@ const Addrecipe = () => {
           <TextField
             required
             fullWidth
+            multiline
+            rows={4}
             name="instructions"
-            label="instructions"
+            label="Instructions"
             variant="outlined"
             margin="normal"
-            // value={recipe.instructions}
-            onChange={inputHandler}
+            value={recipe.instructions}
+            onChange={(e) => {
+              inputHandler(e);
+              handleInstructionsChange(e);
+            }}
             error={errors.instructions}
-            helperText={errors.instructions ? 'instructions is required' : ''}
+            helperText={errors.instructions ? 'Instructions are required' : ''}
             InputLabelProps={{ style: { color: 'white' } }}
             InputProps={styles.textfield}
           />
@@ -135,13 +158,12 @@ const Addrecipe = () => {
             required
             fullWidth
             name="category"
-            label="category"
+            label="Category"
             variant="outlined"
             margin="normal"
-            // value={recipe.category}
             onChange={inputHandler}
             error={errors.category}
-            helperText={errors.category ? 'category is required' : ''}
+            helperText={errors.category ? 'Category is required' : ''}
             InputLabelProps={{ style: { color: 'white' } }}
             InputProps={styles.textfield}
           />
@@ -149,13 +171,12 @@ const Addrecipe = () => {
             required
             fullWidth
             name="image"
-            type='file'
+            type="file"
             variant="outlined"
             margin="normal"
-            // value={recipe.image}
             onChange={inputHandler}
             error={errors.image}
-            helperText={errors.image ? 'image is required' : ''}
+            helperText={errors.image ? 'Image is required' : ''}
             InputLabelProps={{ style: { color: 'white' } }}
             InputProps={styles.textfield}
           />
@@ -166,12 +187,10 @@ const Addrecipe = () => {
           >
             Add Recipe
           </Button>
-          <Box mt={2}>
-          </Box>
         </Box>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Addrecipe
+export default Addrecipe;
