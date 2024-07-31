7a6d7d8aaf7646e920867f324ea30e3ae92fab71
ajaykn_04 <ajaykn04@gmail.com>
2024-08-01 00:37:16 +0530

    push 25

--- frontend/src/components/Myrecipe.jsx
+++ frontend/src/components/Myrecipe.jsx
@@ -31,10 +31,10 @@ const Myrecipe = () => {
       <Grid container spacing={2} sx={{mt:8}}>
       {recipes.map((recipe, index) => (
         <Grid item xs={12} sm={15} md={4} lg={3} key={index}>
-          <Paper elevation={3} sx={{ padding: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '400px'}}>
-            <Container style={{ backgroundColor: 'white', flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Paper elevation={3} sx={{ padding: 1,backgroundColor:'currentcolor', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '400px'}}>
+            <Container style={{ backgroundColor: 'currentcolor', flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <img src={`http://localhost:3000/${recipe.image}`} alt={recipe.name} style={{  width: 'auto', height: '185px', objectFit: 'cover' }} />
-            <Typography variant="h6" sx={{ mt: 1 }}>
+            <Typography variant="h6" fontFamily={'cursive'} sx={{ mt: 1,color:'white', fontWeight:'bold' }}>
               {recipe.name}
             </Typography>
             </Container>
