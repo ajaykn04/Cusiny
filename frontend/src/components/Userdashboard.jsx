38bc861c4e4ad625bebe3a00269fba85f6f06ec6
ajaykn_04 <ajaykn04@gmail.com>
2024-08-03 01:26:49 +0530

    push 35

--- frontend/src/components/Userdashboard.jsx
+++ frontend/src/components/Userdashboard.jsx
@@ -50,7 +50,7 @@ const Userdashboard = () => {
           showIndicators={true}
           swipeable={true}
           centerMode={true}
-          centerSlidePercentage={40}
+          centerSlidePercentage={38}
           selectedItem={selectedIndex}
           onChange={handleSlideChange}
         >
@@ -59,14 +59,14 @@ const Userdashboard = () => {
               key={index}
               style={{
                 padding: '50px',
-                transform: index === selectedIndex ? 'scale(1.2)' : 'scale(1)',
+                transform: index === selectedIndex ? 'scale(1.1)' : 'scale(1)',
                 transition: 'transform 0.5s ease-in-out',
               }}
             >
               <Grid item xs={12} sm={15} md={4} lg={2.3} sx={{ ml: 1.1 }}>
                 <Paper elevation={3} sx={{ padding: 1, backgroundColor: 'currentcolor', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                   <Container style={{ backgroundColor: 'currentcolor', flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                    <Box sx={{ backgroundColor: 'black', height: '42vh', borderRadius: '16px', width: '35vw', boxShadow: '4px 4px 4px rgba(255, 255, 255, 0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
+                    <Box sx={{ backgroundColor: 'black', height: '40vh', borderRadius: '16px', width: '35vw', boxShadow: '4px 4px 4px rgba(255, 255, 255, 0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
                       <Box className="carousel-item" sx={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                         <img src={`http://localhost:3000/${recipe.image}`} style={{ height: 'auto', width: '100%', objectFit: 'cover' }} />
                       </Box>
