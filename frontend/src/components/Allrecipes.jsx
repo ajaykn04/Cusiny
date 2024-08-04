808269224f0f5e9d4fd89534867677daf623cff4
ajaykn_04 <ajaykn04@gmail.com>
2024-08-04 13:05:46 +0530

    sunday push

--- frontend/src/components/Allrecipes.jsx
+++ frontend/src/components/Allrecipes.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 import { Button, Container, Grid, Paper, Rating, Typography } from '@mui/material';
 
