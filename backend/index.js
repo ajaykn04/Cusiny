edf76977e572e1cb95a7b867bfa29db464d14bbd
Adithya550 <adithyavincs23@gmail.com>
2024-08-04 20:31:31 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,12 @@ var userModel = require("./model/user");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'images/recipes/');
+        cb(null, 'images/recipes/');
     },
     filename: (req, file, cb) => {
-      cb(null, crypto.randomBytes(8).toString('hex').slice(0, 8) + path.extname(file.originalname));
+        cb(null, crypto.randomBytes(8).toString('hex').slice(0, 8) + path.extname(file.originalname));
     },
-  });
+});
 const upload = multer({ storage: storage });
 
 
@@ -31,152 +31,165 @@ app.use(cors());
 
 
 
-app.get("/user/viewall", async(req, res)=>{
+app.get("/user/viewall", async (req, res) => {
     try {
         var data = await userModel.find();
         res.send(data)
-        
+
     } catch (error) {
         console.log(error);
     }
 });
 
-app.get("/recipe/viewall", async(req, res)=>{
+app.get("/recipe/viewall", async (req, res) => {
     try {
         var data = await recipeModel.find();
         res.send(data)
-        
+
     } catch (error) {
         console.log(error);
     }
 });
 
-app.get("/recipe/get/:id", async(req, res)=>{
+app.get("/recipe/get/:id", async (req, res) => {
     try {
         var id = req.params.id;
-        var data = await recipeModel.findOne({_id: id});
+        var data = await recipeModel.findOne({ _id: id });
         res.send(data)
-        
+
     } catch (error) {
         console.log(error);
     }
 });
 
-app.post("/user/add", async(req, res)=>{
+app.post("/user/add", async (req, res) => {
     try {
         await userModel(req.body).save();
-        res.send({message: "Data Added"});
+        res.send({ message: "Data Added" });
     } catch (error) {
         console.log(error);
     }
 });
 
-app.post("/recipe/makefeatured/:id", async(req, res)=>{
+app.post("/recipe/makefeatured/:id", async (req, res) => {
     try {
         var id = req.params.id;
         var recipe = await recipeModel.findById(id);
         recipe.featured = true;
         recipe.save();
-        
-        res.send({message: "Added Featured Recipe"})
+
+        res.send({ message: "Added Featured Recipe" })
 
     } catch (error) {
         console.log(error);
     }
 });
 
-app.get("/user/get/:email/:password", async(req, res)=>{
+app.get("/user/get/:email/:password", async (req, res) => {
     try {
         var email = req.params.email;
         var password = req.params.password;
-        var user = await userModel.findOne({email: email, password: password});
-        if (user){
+        var user = await userModel.findOne({ email: email, password: password });
+        if (user) {
             res.send(user);
         }
         else {
             res.status(404);
             res.send({ message: "Invalid Email or Password" });
         }
-        
+
     } catch (error) {
         console.log(error);
     }
 });
 
-app.post("/user/register/", async(req, res)=>{
+app.post("/user/register/", async (req, res) => {
     try {
         var user = req.body;
         user.admin = false;
-        var existing_user = await userModel.findOne({email: user.email});
-        if (!existing_user){
-            
+        var existing_user = await userModel.findOne({ email: user.email });
+        if (!existing_user) {
+
             await userModel(user).save();
-            res.send({message: "Account Registered"});
+            res.send({ message: "Account Registered" });
 
         }
         else {
             res.status(409);
             res.send({ message: "Email Already Exists" });
         }
-        
+
     } catch (error) {
         console.log(error);
     }
 });
 
-app.put("/user/edit/", async(req, res)=>{
+app.put("/user/edit/", async (req, res) => {
     try {
         var id = req.body._id;
         var user = req.body;
-        var existing_user = await userModel.findOne({email: user.email});
+        var existing_user = await userModel.findOne({ email: user.email });
         if (!existing_user) {
             await userModel.findByIdAndUpdate(id, user);
-            res.send({message: "Profile Updated"})
+            res.send({ message: "Profile Updated" })
         }
-        else if (existing_user._id == user._id){
+        else if (existing_user._id == user._id) {
             await userModel.findByIdAndUpdate(id, user);
-            res.send({message: "Profile Updated"})
+            res.send({ message: "Profile Updated" })
         }
         else {
             res.status(409);
-            res.send({message: "Email Already Exists."})
+            res.send({ message: "Email Already Exists." })
         }
-        
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+app.put("/recipe/edit/", upload.single('file'), async (req, res) => {
+    try {
+        var id = req.body._id;
+        var recipe = req.body;
+        var db_recipe = await recipeModel.findById(id);
+        db_recipe.name = recipe.name;
+        db_recipe.ingredients = recipe.ingredients;
+        db_recipe.instructions = recipe.instructions;
+        db_recipe.category = recipe.category;
+        if (!req.file) {
+            await db_recipe.save();
+        } else {
+            var img_path = `${req.file.destination}/${recipe._id}${path.extname(req.file.filename)}`;
+            fs.unlink(db_recipe.image, ()=> {})
+            fs.rename(req.file.path, img_path, () => { })
+            recipe.image = `${img_path}`;
+        }
+        await db_recipe.save();
+        res.send("Recipe Edited");
     } catch (error) {
         console.log(error);
     }
 })
 
-app.delete("/user/delete/", async(req, res)=>{
+app.delete("/recipe/delete/:id", async (req, res) => {
     try {
-        var id = req.body._id;
-        var del = await userModel.findByIdAndDelete(id);
-        if (del != null){
-            res.send({message: "Account Deleted"});
+        var id = req.params.id;
+        var del = await recipeModel.findByIdAndDelete(id);
+        if (del != null) {
+            fs.unlink(del.image, ()=> {});
+            res.send({ message: "Recipe Deleted" });
         }
         else {
             res.status(404);
-            res.send({message: "Failed To Delete Account"});
+            res.send({ message: "Failed To Delete Recipe" });
         }
-        
+
     } catch (error) {
         res.status(404);
-        res.send({message: "Failed To Delete Account"});
+        res.send({ message: "Failed To Delete Recipe" });
     }
 })
 
-app.get("/user/recipes/:id", async(req, res)=>{
-    try {
-        var id = req.params.id;
-        var data = await recipeModel.find({owner: id});
-        res.send(data)
-        
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-app.post("/recipe/add/", upload.single('file'), async(req, res)=>{
+app.post("/recipe/add/", upload.single('file'), async (req, res) => {
     try {
         var recipe = req.body;
         recipe.reviews = [];
@@ -185,22 +198,53 @@ app.post("/recipe/add/", upload.single('file'), async(req, res)=>{
         req.body.image = ""
         recipe = await recipeModel(recipe).save();
         var img_path = `${req.file.destination}/${recipe._id}${path.extname(req.file.filename)}`;
-        fs.rename(req.file.path, img_path, ()=>{})
+        fs.rename(req.file.path, img_path, () => { })
         recipe.image = `${img_path}`;
         recipe.save();
-        
-        res.send({message: "Recipe Added"})
+
+        res.send({ message: "Recipe Added" })
 
     } catch (error) {
         console.log(error);
     }
 });
 
-app.get("/recipe/featured", async(req, res)=>{
+app.delete("/user/delete/", async (req, res) => {
     try {
-        var data = await recipeModel.find({featured: true});
+        var id = req.body._id;
+        var del = await userModel.findByIdAndDelete(id);
+        if (del != null) {
+            res.send({ message: "Account Deleted" });
+        }
+        else {
+            res.status(404);
+            res.send({ message: "Failed To Delete Account" });
+        }
+
+    } catch (error) {
+        res.status(404);
+        res.send({ message: "Failed To Delete Account" });
+    }
+})
+
+app.get("/user/recipes/:id", async (req, res) => {
+    try {
+        var id = req.params.id;
+        var data = await recipeModel.find({ owner: id });
         res.send(data)
-        
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+app.get("/recipe/featured", async (req, res) => {
+    try {
+        var data = await recipeModel.find({ featured: true });
+        res.send(data)
+
     } catch (error) {
         console.log(error);
     }
@@ -208,6 +252,6 @@ app.get("/recipe/featured", async(req, res)=>{
 app.use('/images/recipes', express.static(path.join(__dirname, 'images/recipes')));
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Port is Up");
 });
