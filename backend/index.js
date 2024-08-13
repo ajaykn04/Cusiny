0202562302549f0a00e4703836efe70d43b4c72d
Ajay Das <134246618+ajaykn04@users.noreply.github.com>
2024-08-13 21:06:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,11 @@ const upload = multer({ storage: storage });
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 
 
