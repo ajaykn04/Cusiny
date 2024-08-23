ce9c8d4afaf90fcadfb7f5d6d918d2c241af9660
Ajay Das <134246618+ajaykn04@users.noreply.github.com>
2024-08-24 00:44:05 +0530

    Add files via upload

---
+++ backend/connection.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected!");
+}).catch((error) => {
+    console.log(error)
+})
